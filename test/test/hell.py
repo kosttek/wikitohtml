46e3a9e2a24f9eb5505a5bc6ff00017b8de52a41
Marcin K <krzesigniew@gmail.com>
2012-03-29 00:16:53 +0200

    xml parser begin and load from file

--- test/test/hell.py
+++ test/test/hell.py
@@ -3,6 +3,28 @@ Created on 16-03-2012
 
 @author: kosttek
 '''
-print 'hellka'
+import sys
+from xml.etree.ElementTree import ElementTree
+
+class LoadFile:
+    def read(self,path):
+        try:
+            f = open(path,'r')
+            print f
+        except IOError:
+            print 'There is no file like this, Joseph is disappointed'
+    def readXMLosoby(self):
+        tree = ElementTree()
+        tree.parse('../data/some_data.xml')
+        root = tree.getroot()
+        osoby = root.iter('osoba')
+        for osoba in osoby:
+            print osoba.attrib
+            for dane in osoba.iter():
+                print dane.tag, dane.text,  dane.iter()
+
 if __name__ == '__main__':
     print 'hell'
+    print sys.__class__
+    x = LoadFile()
+    x.readXMLosoby()
