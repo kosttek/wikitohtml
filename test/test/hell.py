195b3f41d5a225323a358c880b676c0adc284be3
Marcin K <krzesigniew@gmail.com>
2012-04-03 23:42:05 +0200

    test scanner begining

--- test/test/hell.py
+++ test/test/hell.py
@@ -13,18 +13,48 @@ class LoadFile:
             print f
         except IOError:
             print 'There is no file like this, Joseph is disappointed'
+            
     def readXMLosoby(self):
         tree = ElementTree()
         tree.parse('../data/some_data.xml')
         root = tree.getroot()
+        print dir(root)
         osoby = root.iter('osoba')
         for osoba in osoby:
             print osoba.attrib
             for dane in osoba.iter():
                 print dane.tag, dane.text,  dane.iter()
+                
+    def readXML2(self):
+        tree = ElementTree()
+        tree.parse('../data/wikitree.xml')
+        root = tree.getroot()
+        print dir(root)
+        elemelki = list(root)
+        for elemelek in elemelki:
+            print elemelek
+
+class Scanner:
+    tree = 0
+    file = 0
+    treeElement = 0
+    
+    def loadTree(self,path):
+        self.tree = ElementTree()
+        self.tree.parse(path)
+    
+    def loadFile(self,path):
+        self.file = open(path,'r')
+        
+    def scan(self):
+        treeElement = self.tree.getroot()
+        for character in self.file.read() :
+            print character
+        # stack .pop() .append()
+        # trzeba ogarniac czy znacznik jest juz zamkniety czy jeszcze nie niestety
 
 if __name__ == '__main__':
-    print 'hell'
-    print sys.__class__
-    x = LoadFile()
-    x.readXMLosoby()
+    scanner = Scanner()
+    scanner.loadTree('../data/wikitree.xml')
+    scanner.loadFile('../data/testfile')
+    scanner.scan()
