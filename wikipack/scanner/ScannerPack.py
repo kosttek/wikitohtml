ecab62694880458007c73524bb5dfb08d4f13d14
Marcin K <krzesigniew@gmail.com>
2012-04-14 18:05:04 +0200

    scanner do troche do przodu

--- wikipack/scanner/ScannerPack.py
+++ wikipack/scanner/ScannerPack.py
@@ -13,44 +13,53 @@ class Scanner:
     file = 0
     treeElement = 0
     
-    def loadTree(self,path):
+    def loadTree(self, path):
         self.tree = ElementTree()
         self.tree.parse(path)
     
-    def loadFile(self,path):
-        self.file = open(path,'r')
+    def loadFile(self, path):
+        self.file = open(path, 'r')
         
     
         
     def scan(self):
         treeElement = self.tree.getroot()
         for line in self.file.readlines() : 
-            lineMark =  self.checkLineStartEndMarks(line)
-            self.scanLine(line)
+            lineMark = self.checkLineStartEndMarks(line)
+            print self.scanLine(line)
+            
             print "---------------------------"
             
-    def scanLine(self,line):
-        ret = list()
+    def scanLine(self, line):
+        class Namespace: pass
         
         def setEndMark(vo):
-            print "return - >",vo.retMark
+            #print "return - >",vo.retMark
+            ret.append(vo.tempString)
+            vo.tempString = ""
+            ret.append(vo.markAcumulator) # ret.append(vo.retMark)
+            vo.markAcumulator = ""
             vo.treeElement = vo.root
-            vo.retMark =0
-        
+            vo.retMark = 0
+            
         class varObject:
             #TODO sprobowac z find
             treeElement = self.tree.getroot().iter("chars").next() 
             root = treeElement
             retMark = 0
             
+        ret = list()
         vo = varObject()
+        vo.tempString = ""
+        vo.markAcumulator = ""
+        
         for character in line:
             
             for element in list(vo.treeElement):
                 
-                if element.attrib["value"]==character:
-                    
-                    vo.retMark = element.get("return",0)
+                if element.attrib["value"] == character:
+                    vo.markAcumulator = vo.markAcumulator + character
+                    vo.retMark = element.get("return", 0)
                     if len(list(element)) == 0:
                         setEndMark(vo) 
                     else:
@@ -60,26 +69,30 @@ class Scanner:
                     setEndMark(vo)
                     break
                 else:
+                    vo.tempString = vo.tempString + vo.markAcumulator + character
+                    vo.markAcumulator = ""
                     vo.treeElement = vo.root
-                    vo.retMark =0 
+                    vo.retMark = 0 
                     
-                    
+        ret.append(vo.tempString)
+        
+        return ret            
 
                     
             
             
-    def checkLineStartEndMarks(self,line):
-        if len(line)>2:
+    def checkLineStartEndMarks(self, line):
+        if len(line) > 2:
             treeElement = self.tree.getroot()
             for element in treeElement.iter("linemark"): 
                 if element.attrib['value'] == line[0] and element.attrib['value'] == line[-2]:
                     mark = element.attrib['value']
-                    for i in range(1,int(element.attrib['max'])):
-                        if mark == line[i] and mark == line[-2-i]:
+                    for i in range(1, int(element.attrib['max'])):
+                        if mark == line[i] and mark == line[-2 - i]:
                             pass
                         else:
                             return {i, mark}
-        return {0,""}
+        return {0, ""}
     
 if __name__ == '__main__':
     scanner = Scanner()
--- wikipack/tests/ScannerTest.py
+++ wikipack/tests/ScannerTest.py
@@ -4,17 +4,18 @@ Created on 07-04-2012
 @author: kosttek
 '''
 import unittest
-import scanner.ScannerPack
+import wikipack.scanner.ScannerPack
 
 class TestScanner(unittest.TestCase):
     scanner = 0
     def setUp(self):
-        self.scanner= scanner.ScannerPack.Scanner()
-        self.scanner.loadTree('../data/wikitree.xml')
+        self.scanner= wikipack.scanner.ScannerPack.Scanner()
+        self.scanner.loadTree('../data/wikitree2.xml')
 
     def testScanLineApostroph(self):
-        ret = self.scanner.scanLine("ala ma 'kota' ale ''kot'' nie ma '''ali mc bill''' tak nie \n");
+        ret = self.scanner.scanLine("ala ma 'kota' ale ''kot'' nie ma '''ali mc bill'''' tak nie \n");
         self.assertEqual(len(ret), 9, "dlugosc listy")
+        #TODO rozszerzyc testy 
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
