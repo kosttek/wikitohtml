b409902a1546b9d4970e2486fc15be5d82fe2017
PepcokPL <kwiatkowski.przemysl+github@gmail.com>
2012-05-19 13:46:01 +0200

    Parser tree - szablon klasy i testy

--- wikipack/parser/ParserTreeHelper.py
+++ wikipack/parser/ParserTreeHelper.py
@@ -3,20 +3,31 @@ Created on 19-05-2012
 
 @author: Pepcok
 '''
+from xml.etree.ElementTree import ElementTree
+import os
 
 class ParserTreeHelper(object):
-    '''
-    classdocs
-    '''
+    
+    __xmlTreePath = 'parserTree.xml'
+    __tokenTree = None
 
 
     def __init__(self):
-        pass
-
+        self.__tokenTree = ElementTree()
+        self.__tokenTree.parse(os.path.dirname(__file__) + '\\' + self.__xmlTreePath)
+        
     def isTokenATag(self, tokenSymbol):
+        tokenList = self.__tokenTree.findall('token')
+        for token in tokenList:
+            if tokenSymbol == token.find('startTag').text:
+                return True
         return False;
     
     def getEndingTag(self, tokenSymbol):
         return None
     
     
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+   parser = ParserTreeHelper()
+   parser.isTokenATag("'''")
